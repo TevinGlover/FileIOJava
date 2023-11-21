9035e5dfe0854f0edf581015a024cf61b59e79c3
Tevin Glover <tevwork.tg@gmail.com>
2023-11-20 23:10:21 -0500

    ok

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,7 @@
 package Step4;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -10,9 +12,31 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             // Print out a running total of all the
-            // values in the input file.
+            try {
+                // try catch
+                PrintWriter fileOut = new PrintWriter("out.txt");
+
+                fileOut.println("hello");
+
+                fileOut.close();
+
+                System.out.println("done");
+
+                Scanner fileIn = new Scanner((new File("output.txt")));
+                while (fileIn.hasNext()) {
+                    String lineIn = fileIn.nextLine();
+                }
+                System.out.println("done!");
+            } catch (FileNotFoundException ex) {
+                throw new RuntimeException(ex);
+            }
         }
-}
+
+
+        
+    }
+
+
+            // values in the input file.
